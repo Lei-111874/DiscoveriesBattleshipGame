3bbb114550d3cb083ada2dd29e39830ce56c3648
Fábio do Rosário <fovdr@iscte-iul.pt>
2026-09-25 15:28:54 +0100

    Update Barge.java

--- src/main/java/iscteiul/ista/battleship/Barge.java
+++ src/main/java/iscteiul/ista/battleship/Barge.java
@@ -1,21 +1,26 @@
-/**
- *
- */
+/** 
+* Representa uma barca no jogo Batalha Naval. 
+* Uma barca ocupa apenas uma posição no tabuleiro. 
+*/
 package iscteiul.ista.battleship;
 
 public class Barge extends Ship {
     private static final Integer SIZE = 1;
     private static final String NAME = "Barca";
 
-    /**
-     * @param bearing - barge bearing
-     * @param pos     - upper left position of the barge
-     */
+   /** * Cria uma nova barca com a orientação e a posição indicadas. 
+   * 
+   * @param bearing orientação da barca 
+   * @param pos posição superior esquerda da barca 
+   */
     public Barge(Compass bearing, IPosition pos) {
         super(Barge.NAME, bearing, pos);
         getPositions().add(new Position(pos.getRow(), pos.getColumn()));
     }
-
+   /** * Obtém o tamanho da barca. 
+   * 
+   * @return tamanho da barca, que é sempre 1 
+   */
     @Override
     public Integer getSize() {
         return SIZE;
